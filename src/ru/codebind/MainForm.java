f7f8e85c1eedfa44355a438dbb5c714fe77f0681
Ki10V01T <ampernaom@gmail.com>
2019-06-07 00:05:08 +0300

    Initial commit

---
+++ src/ru/codebind/MainForm.java
@@ -0,0 +1,43 @@
+package ru.codebind;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainForm extends JFrame{
+
+    private JButton helpButton;
+    private JButton exitButton;
+    private JPanel rootPanel;
+
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+
+    }
+
+    private void Init(){
+        setContentPane(rootPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+    }
+
+    private MainForm() {
+        Init();
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+        MainForm mf = new MainForm();
+
+    }
+}
